6fc437cb28c85c9ba745a284f47f44475e4e170b
Swaleha begum Mulla <120295486+swaleha2002@users.noreply.github.com>
2025-03-15 13:57:39 +0530

    Add files via upload

---
+++ first query.sql
@@ -0,0 +1,6 @@
+-- Create View for Power BI
+Create View vw_ChurnData as
+    select * from prod_Churn where Customer_Status In ('Churned','Stayed');
+
+Create View vw_JoinData as
+    select * from prod_Churn where Customer_Status = 'Joined';
